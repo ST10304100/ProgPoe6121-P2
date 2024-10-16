d83a7a6dca9b910fc941faffca8cd1a37d1bfd12
Jordan Gardiner <ST10304100@vcconnect.edu.za>
2024-10-16 10:24:15 +0100

    Add project files.

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PROG_PART_2.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+       
+            public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+            {
+            }
+           
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROG_PART_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
